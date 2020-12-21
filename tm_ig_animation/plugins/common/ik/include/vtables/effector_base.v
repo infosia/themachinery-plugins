6ea377864d036825a15622d80bab3b0f1588d2b6
Kota Iguchi <developer@infosia.co.jp>
2020-12-21 19:49:09 +0900

    remove ik because of linkage issues

--- tm_ig_animation/plugins/common/ik/include/vtables/effector_base.v
+++
@@ -1,3 +0,0 @@
-#include "ik/effector.h"
-
-IK_IMPLEMENT(effector_base, effector_interface)
--- tm_ig_animation/plugins/inverse_kinematics/animation_inverse_kinematics.h
+++ tm_ig_animation/plugins/inverse_kinematics/animation_inverse_kinematics.h
@@ -2,11 +2,11 @@
 
 #include <foundation/api_types.h>
 
-#define TM_TT_TYPE__IG_ANIMATION_PUPPET "tm_ig_animation_puppet"
-#define TM_TT_TYPE_HASH__IG_ANIMATION_PUPPET TM_STATIC_HASH("tm_ig_animation_puppet",  0x417d8f2cd6fd4f98ULL)
+#define TM_TT_TYPE__IG_ANIMATION_INVERSE_KINEMATICS "tm_ig_animation_inverse_kinematics"
+#define TM_TT_TYPE_HASH__IG_ANIMATION_INVERSE_KINEMATICS TM_STATIC_HASH("tm_ig_animation_inverse_kinematics",  0x725c672fece6864aULL)
 
 enum {
-    TM_TT_PROP__IG_ANIMATION_PUPPET__POSITION_FACTOR, // subobject(TM_TT_TYPE__POSITION)
-    TM_TT_PROP__IG_ANIMATION_PUPPET__ROTATION_FACTOR, // subobject(TM_TT_TYPE__ROTATION)
-    TM_TT_PROP__IG_ANIMATION_PUPPET__SCALE_FACTOR,    // subobject(TM_TT_TYPE__SCALE)
+    TM_TT_PROP__IG_ANIMATION_IK__POSITION_FACTOR, // subobject(TM_TT_TYPE__POSITION)
+    TM_TT_PROP__IG_ANIMATION_IK__ROTATION_FACTOR, // subobject(TM_TT_TYPE__ROTATION)
+    TM_TT_PROP__IG_ANIMATION_IK__SCALE_FACTOR,    // subobject(TM_TT_TYPE__SCALE)
 };
